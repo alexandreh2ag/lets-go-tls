196e8a79b86e2e3d4b58610135058954321c56bf
alexandreh2ag <alexandre.haag90@gmail.com>
2025-01-19 18:28:05 +0100

    Add support to listen TLS
    Signed-off-by: alexandreh2ag <alexandre.haag90@gmail.com>

--- apps/server/http/server_test.go
+++ apps/server/http/server_test.go
@@ -3,6 +3,9 @@ package http
 import (
 	"github.com/alexandreh2ag/lets-go-tls/apps/server/acme/http"
 	appCtx "github.com/alexandreh2ag/lets-go-tls/apps/server/context"
+	appProm "github.com/alexandreh2ag/lets-go-tls/prometheus"
+	"github.com/alexandreh2ag/lets-go-tls/types"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -12,9 +15,27 @@ func TestCreateServerHTTP(t *testing.T) {
 	ctx := appCtx.TestContext(nil)
 	ctx.Config.HTTP.Listen = "127.0.0.1:0"
 	ctx.Config.HTTP.MetricsEnable = true
+	ctx.MetricsRegister = appProm.NewRegistry(types.NameServerMetrics, prometheus.NewRegistry())
 	httpProvider := http.NewChallenge(ctx.Logger, ctx.Cache)
 	got, err := CreateServerHTTP(ctx, httpProvider)
 	time.Sleep(200 * time.Millisecond)
 	assert.NoError(t, err)
 	assert.NotNil(t, got)
+}
+
+func TestCreateServerHTTPWithTLS_Fail(t *testing.T) {
+	ctx := appCtx.TestContext(nil)
+	ctx.Config.HTTP.Listen = "127.0.0.1:0"
+	ctx.Config.HTTP.MetricsEnable = true
+	ctx.MetricsRegister = appProm.NewRegistry(types.NameServerMetrics, prometheus.NewRegistry())
+	ctx.Config.HTTP.TLS.Enable = true
+	ctx.Config.HTTP.TLS.CertPath = "./fixtures/cert.crt"
+	ctx.Config.HTTP.TLS.KeyPath = "./fixtures/priv.key"
+	httpProvider := http.NewChallenge(ctx.Logger, ctx.Cache)
+
+	got, err := CreateServerHTTP(ctx, httpProvider)
+	time.Sleep(200 * time.Millisecond)
+	assert.NoError(t, err)
+	assert.NotNil(t, got)
+
 }
