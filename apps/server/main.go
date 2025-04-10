0a495b8601fa8f35dc8b311d84340b7dd4834737
alexandreh2ag <alexandre.haag90@gmail.com>
2025-04-10 15:56:33 +0200

    Remove dead code
    Signed-off-by: alexandreh2ag <alexandre.haag90@gmail.com>

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/alexandreh2ag/lets-go-tls/apps/server/cli"
 	"github.com/alexandreh2ag/lets-go-tls/apps/server/context"
-	"os"
-	"time"
 )
 
 func main() {
-	expire := time.Date(2025, 04, 25, 13, 00, 00, 00, time.UTC)
-	dueDate := time.Now().Add(time.Hour * 24 * 30 * -1)
-
-	fmt.Println("expire", expire)
-	fmt.Println("due date", dueDate)
-	fmt.Println(expire.After(dueDate))
-	os.Exit(0)
 	ctx := context.DefaultContext()
 	rootCmd := cli.GetRootCmd(ctx)
 
